ac8df040633f737dff525bb5188780bcb4623652
Adam <A.ergashev@internet.ru>
2023-08-21 01:56:33 +0300

    ADD: get_sorted_operation
    - FIX: get_executed
    - ADD: get_sorted_operation

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import json
 
+from classes import Operation
+
 
 def load_file(filename):
     '''
@@ -11,12 +13,31 @@ def load_file(filename):
         return json.load(f)
 
 
-def get_executed(operation_list):
+def get_executed(operation_list:list[dict]):
     '''
-
-    :param operation_list: список
-    :return: выполненные операции
+    Поулчаем список ВЫПОЛНЕННЫХ операций
+    :param operation_list: список словарей с данными об операции
+    :return: список элементов класса Operation
     '''
     operation_list = [op for op in operation_list if op and op["state"] == "EXECUTED"]
-    return operation_list
+    return [
+        Operation(
+            op["date"],
+            op["description"],
+            op["from"],
+            op["to"],
+            op["operationAmount"]["amount"],
+            op["operationAmount"]["currency"]["name"]
+        )
+        for op in operation_list
+    ]
+
+
+def get_sorted_operations(operation_list:list[Operation]):
+    '''
+
+    :param operation_list: список элементов класса Operation
+    :return: отсортированный список по дате
+    '''
+    return sorted(operation_list, key=lambda op: op.get_datetime(), reverse=True)
 
