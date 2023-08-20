2127b9fdc007de0db244470ec1dfb3a435738c1a
Adam <A.ergashev@internet.ru>
2023-08-21 00:12:04 +0300

    ADD: get_executed(func)

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_file(filename):
     '''
 
@@ -9,4 +10,13 @@ def load_file(filename):
     with open(filename, encoding="UTF-8", mode="r") as f:
         return json.load(f)
 
+
+def get_executed(operation_list):
+    '''
+
+    :param operation_list: список
+    :return: выполненные операции
+    '''
+    operation_list = [op for op in operation_list if op and op["state"] == "EXECUTED"]
+    return operation_list
 
