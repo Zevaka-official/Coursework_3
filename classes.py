8a7d667bcf5dff87867b774cebedd9112ac84617
Adam <A.ergashev@internet.ru>
2023-08-22 15:05:17 +0300

    REFACTORING: classes.py
    - add: docstrings

--- classes.py
+++ classes.py
@@ -14,13 +14,26 @@ class Operation():
         return f"{self.date} {self.description} {self.op_from} {self.op_to} {self.amount} {self.currency}"
 
     def get_convert_date(self):
+        """
+
+        :return: возвращает конвертированную дату
+        """
         return self.date.strftime("%d.%m.%Y")
 
     def get_description(self):
+        """
+
+        :return: возвращает детали операции
+        """
         return self.description
 
     @staticmethod
     def _get_from(input_str: str):
+        """
+
+        :param input_str: получает детали операции
+        :return: возвращает маску операции
+        """
         new_list = input_str.split()
         letters = new_list[0:-1]
         number = new_list[-1]
@@ -30,16 +43,32 @@ class Operation():
             return f'Счет **{number[-4:]}'
 
     def get_from(self):
+        """
+
+        :return: возвращает детали отправителя
+        """
         if self.op_from is None:
             return "Нет данных"
 
         return Operation._get_from(self.op_from)
 
     def get_to(self):
+        """
+
+        :return: возвращает детали получателя
+        """
         return Operation._get_from(self.op_to)
 
     def get_amount(self):
+        """
+
+        :return: возвращает сумму и валюту
+        """
         return f'{self.amount} {self.currency}'
 
     def get_datetime(self):
+        """
+
+        :return: возвращает дату
+        """
         return self.date
